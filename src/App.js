e42bd9b17f998dce0f7f4d2672d8f4ac51c46f09
Leonardo Souza <leogsouza@gmail.com>
2017-07-27 10:59:04 -0300

    Formatting

--- src/App.js
+++ src/App.js
@@ -5,32 +5,38 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import fusTheme from './fusTheme';
+
 injectTapEventPlugin();
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {open: false}
+    this.state = { open: false }
   };
 
   render() {
     return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
-        />
-        <Drawer
-          open={this.state.open}
-          docked={false}
-          onRequestChange={(open) => this.setState({open})}
-        >
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
+          />
+          <Drawer
+            open={this.state.open}
+            docked={false}
+            onRequestChange={(open) => this.setState({ open })}
+          >
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
