980b0b51a541b89399a012cbdae61abe00b036ff
Leonardo Souza <leogsouza@gmail.com>
2017-07-27 16:53:08 -0300

    Refactoring App

--- src/App.js
+++ src/App.js
@@ -1,106 +1,54 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css'
-import { BrowserRouter, Route, NavLink } from 'react-router-dom'
-import AppBar from 'material-ui/AppBar'
-import Drawer from 'material-ui/Drawer'
-import MenuItem from 'material-ui/MenuItem'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import fusTheme from './fusTheme'
 
+import SideNav from './SideNav'
+
+/* Import Views */
+import Glossary from './views/Glossary'
+import Home from './views/Home'
+import Letterhead from './views/Letterhead'
+import Logos from './views/Logos'
+import LogosPosters from './views/LogosPosters'
+import PlanningGuide from './views/PlanningGuide'
+import Posters from './views/Posters'
+import ServiceRequest from './views/ServiceRequest'
+import Services from './views/Services'
+import Story from './views/Story'
+import Tutorial from './views/Tutorial'
+
 injectTapEventPlugin()
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { open: false }
-  }
-
-  handleClose = () => this.setState({ open: false })
-
-  SideBarItem = ({ link, text, isExact }) => {
-    return (
-      <NavLink exact={isExact} to={link}>
-        <MenuItem onTouchTap={this.handleClose} primaryText={text} />
-      </NavLink>
-    )
-  }
-
   render () {
-    const SideBarItem = this.SideBarItem
     return (
       <BrowserRouter>
         <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
           <div>
-            <AppBar
-              title='Title'
-              iconClassNameRight='muidocs-icon-navigation-expand-more'
-              onLeftIconButtonTouchTap={() =>
-                this.setState({ open: !this.state.open })}
-            />
-            <Drawer
-              open={this.state.open}
-              docked={false}
-              onRequestChange={open => this.setState({ open })}
-            >
-              <SideBarItem link='/' text='Home' isExact />
-              <SideBarItem link='/logos' text='Logos' isExact />
-              <SideBarItem link='/posters' text='Posters' isExact />
-              <SideBarItem link='/letterhead' text='Letterhead' isExact />
-              <SideBarItem link='/share-a-story' text='Share a Story' isExact />
-              <SideBarItem
-                link='/service-request-form'
-                text='Service Request Form'
-                isExact
-              />
-              <SideBarItem link='/Tutorial' text='Tutorial' isExact />
-            </Drawer>
+            <SideNav />
 
-            <Route exact path='/' render={() => <h1>Home View</h1>} />
-            <Route
-              exact
-              path='/logos-posters'
-              render={() => <h1>Logo and Posters View</h1>}
-            />
-            <Route exact path='/logos' render={() => <h1>Logos View</h1>} />
-            <Route exact path='/posters' render={() => <h1>Posters View</h1>} />
-            <Route
-              exact
-              path='/letterhead'
-              render={() => <h1>Letterhead View</h1>}
-            />
-            <Route
-              exact
-              path='/share-a-story'
-              render={() => <h1>Share a Story View</h1>}
-            />
-            <Route
-              exact
-              path='/planning-guide'
-              render={() => <h1>Planning Guide View</h1>}
-            />
-            <Route
-              exact
-              path='/services'
-              render={() => <h1>Services View</h1>}
-            />
-            <Route
-              exact
-              path='/glossary'
-              render={() => <h1>Glossary View</h1>}
-            />
-            <Route
-              exact
-              path='/service-request-form'
-              render={() => <h1>Service Request Form View</h1>}
-            />
-            <Route
-              exact
-              path='/tutorial'
-              render={() => <h1>Tutorial View</h1>}
-            />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/logos-posters' component={LogosPosters} />
+              <Route exact path='/logos' component={Logos} />
+              <Route exact path='/posters' component={Posters} />
+              <Route exact path='/letterhead' component={Letterhead} />
+              <Route exact path='/share-a-story' component={Story} />
+              <Route exact path='/planning-guide' component={PlanningGuide} />
+              <Route exact path='/services' component={Services} />
+              <Route exact path='/glossary' component={Glossary} />
+              <Route
+                exact
+                path='/service-request-form'
+                component={ServiceRequest}
+              />
+              <Route exact path='/tutorial' component={Tutorial} />
+            </Switch>
           </div>
         </MuiThemeProvider>
       </BrowserRouter>
