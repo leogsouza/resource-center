789d885e0d81b134a51a0c7289c23d009c994326
Leonardo Souza <leogsouza@gmail.com>
2017-08-01 11:12:49 -0300

    Create element to allow customize hover event

--- src/SideBarItem.js
+++ src/SideBarItem.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import MenuItem from 'material-ui/MenuItem'
+import { ListItem } from 'material-ui/List'
 
 class SideBarItem extends Component {
   constructor (props) {
     super(props)
-    this.state = { open: false }
+    this.state = { open: false, click: false }
   }
 
   render () {
@@ -15,10 +15,10 @@ class SideBarItem extends Component {
         to={this.props.linkTo}
         key={this.props.i}
       >
-        <MenuItem
+        <ListItem
           onTouchTap={this.props.handleClose}
           primaryText={this.props.primaryText}
-          hoverColor='#fff'
+          hoverColor='lightsteelblue'
         />
       </NavLink>
     )
