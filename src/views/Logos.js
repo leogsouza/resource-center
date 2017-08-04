8e4e9668ac1a3cc281b8bd71662c61327ca1d142
Leonardo Souza <leogsouza@gmail.com>
2017-08-04 15:58:05 -0300

    Add generic card

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import { GenericCard } from '../components/GenericCard'
+import { CardTitle } from 'material-ui/Card'
 
 class Logos extends Component {
   render () {
-    return <h1>Logos View</h1>
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col m6'>
+            <GenericCard
+              headerTitle='Hello'
+              headerAvatar='http://via.placeholder.com/100x200'
+              mediaImgSrc='http://via.placeholder.com/120x100'
+              overlay={
+                <CardTitle
+                  title='Nice Job'
+                  subtitle='Congratulations on this great Achievement'
+                />
+              }
+              cardTitle='Nice one'
+              cardSubtitle='Looking good'
+            >
+              Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz
+              malandris se pirulitá. Mais vale um bebadis conhecidiss, que um
+              alcoolatra anonimis. Leite de capivaris, leite de mula manquis sem
+              cabeça. Delegadis gente finis, bibendum egestas augue arcu ut est.
+            </GenericCard>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
