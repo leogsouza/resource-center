343fa373ca02194af4b7e044f2f53d477cc4a332
Leonardo Souza <leogsouza@gmail.com>
2017-08-09 23:05:59 -0300

    Add cardActions using actions prop

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { GenericCard } from '../components/GenericCard'
 import { CardTitle } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 class Logos extends Component {
   render () {
     return (
       <div className='container'>
         <div className='row'>
-          <div className='col m6'>
+          <div className='col s12 m6'>
             <GenericCard
               headerTitle='Hello'
               headerAvatar='http://via.placeholder.com/100x200'
@@ -20,6 +21,12 @@ class Logos extends Component {
               }
               cardTitle='Nice one'
               cardSubtitle='Looking good'
+              actions={
+                <div>
+                  <FlatButton label='Action1' />
+                  <FlatButton label='Action2' />
+                </div>
+              }
             >
               Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz
               malandris se pirulitá. Mais vale um bebadis conhecidiss, que um
